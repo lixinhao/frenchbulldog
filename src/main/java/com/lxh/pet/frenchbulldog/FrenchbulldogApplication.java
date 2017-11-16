72824b42df0ff90514e85dff310aab127be51d07
lixinhao <743401095@qq.com>
2017-11-16 21:29:37 +0800

    Initial commit

---
+++ src/main/java/com/lxh/pet/frenchbulldog/FrenchbulldogApplication.java
@@ -0,0 +1,12 @@
+package com.lxh.pet.frenchbulldog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenchbulldogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrenchbulldogApplication.class, args);
+	}
+}
